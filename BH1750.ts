6332730c5f8e25166c31bf07ef7387bbd068a07f
birchie liu <56035255+teenkit@users.noreply.github.com>
2019-12-06 15:00:01 +0800

    Update BH1750.ts

--- BH1750.ts
+++ BH1750.ts
@@ -1,63 +1,45 @@
 /**
  * makecode BH1750 Digital Ambient Light Sensor Package.
- * From microbit/micropython Chinese community.
- * http://www.micropython.org.cn
+ * 
+ * http://www.rootbrother.com
  */
 
-// BH1750's I2C address
-enum BH1750_ADDRESS {
-    //% block="35"
-    A35,
-    //% block="92"
-    A92
-}
 
-let Address = 35
+let Address = 92
 
 /**
  * BH1750 Digital Ambient Light Sensor Package
  */
-//% weight=100 color=#000011 icon="\uf185"
+//% weight=100 color=#ffc107 icon="\uf185" block="teenkit光照传感器"
 namespace BH1750 {
-    /**
-     * set BH1750 Digital Ambient Light Sensor I2C address, default is 35
-     * @param is I2C address, eg: 35
-     */
-    //% blockId="BH1750_SET_ADDRESS" block="set Address %addr"
-    //% weight=100 blockGap=8
-    export function SetAddress(addr: BH1750_ADDRESS): void {
-        if (addr == BH1750_ADDRESS.A35)
-            Address = 35
-        else
-            Address = 92
-    }
+
 
     /**
-     * turn on BH1750.
+     * 开启光照强度传感器
      */
-    //% blockId="BH1750_ON" block="turn on"
+    //% blockId="BH1750_ON" block="开启光照传感器"
     //% weight=90 blockGap=8
     export function on(): void {
         pins.i2cWriteNumber(Address, 0x10, NumberFormat.UInt8BE)
     }
 
     /**
-     * turn off BH1750, to reduce power consumption.
+     * 关闭光照强度传感器
      */
-    //% blockId="BH1750_OFF" block="turn off"
+    //% blockId="BH1750_OFF" block="关闭光照传感器"
     //% weight=90 blockGap=8
     export function off(): void {
         pins.i2cWriteNumber(Address, 0, NumberFormat.UInt8BE)
     }
 
     /**
-     * get ambient light data (lx)
+     * 读取环境光数值，单位：勒克斯（lx）
      */
-    //% blockId="BH1750_GET_INTENSITY" block="get intensity (lx)"
+    //% blockId="BH1750_GET_INTENSITY" block="光线强度"
     //% weight=80 blockGap=8
     export function getIntensity(): number {
         return Math.idiv(pins.i2cReadNumber(Address, NumberFormat.UInt16BE) * 5, 6)
     }
 
-    on();
-}  
+
+}   
